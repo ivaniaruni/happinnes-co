bcbb807d56937b35749fe105ec63a937b08f0128
Ivan Iarunichev <iarunichevivan@gmail.com>
2025-04-01 11:01:41 +0200

    change 25.04.01.02

--- js/slider-script.js
+++ js/slider-script.js
@@ -1,3 +1,4 @@
+/* Home */
 document.addEventListener("DOMContentLoaded", function () {
     const slider = document.querySelector(".images");
     const images = document.querySelectorAll(".image-item");
@@ -33,6 +34,7 @@ document.addEventListener("DOMContentLoaded", function () {
     updateSlider();
 });
 
+/* Upcoming Events */
 document.addEventListener("DOMContentLoaded", function () {
     const upcomingSliderContainer = document.querySelector(".gallery-upcoming");
     const upcomingSlider = upcomingSliderContainer.querySelector(".images-upcoming");
@@ -47,7 +49,7 @@ document.addEventListener("DOMContentLoaded", function () {
     function updateUpcomingSlider() {
         const imageWidth = upcomingImages[0].offsetWidth; 
         const totalWidth = imageWidth * upcomingImages.length;
-        upcomingSlider.style.transform = `translateX(${-currentIndexUpcoming * imageWidth}px)`; // Desplazamiento basado en el índice actual
+        upcomingSlider.style.transform = `translateX(${-currentIndexUpcoming * imageWidth}px)`;
     }
 
     nextUpcomingButton.addEventListener("click", function () {
@@ -66,4 +68,40 @@ document.addEventListener("DOMContentLoaded", function () {
 
     window.addEventListener("resize", updateUpcomingSlider);
     updateUpcomingSlider();
+});
+
+/* Event History */
+document.addEventListener("DOMContentLoaded", function () {
+    const historySliderContainer = document.querySelector(".gallery-history");
+    const historySlider = historySliderContainer.querySelector(".images-event-history");
+    const historyImages = historySliderContainer.querySelectorAll(".image-item-event-history");
+    const prevHistoryButton = historySliderContainer.querySelector(".prev-event-history");
+    const nextHistoryButton = historySliderContainer.querySelector(".next-event-history");
+
+    let currentIndexHistory = 0;
+
+    const visibleImagesHistory = 3;
+
+    function updateHistorySlider() {
+        const imageWidth = historyImages[0].offsetWidth; 
+        const totalWidth = imageWidth * historyImages.length;
+        historySlider.style.transform = `translateX(${-currentIndexHistory * imageWidth}px)`;
+    }
+
+    nextHistoryButton.addEventListener("click", function () {
+        if (currentIndexHistory < historyImages.length - visibleImagesHistory) {
+            currentIndexHistory++;
+            updateHistorySlider();
+        }
+    });
+
+    prevHistoryButton.addEventListener("click", function () {
+        if (currentIndexHistory > 0) {
+            currentIndexHistory--;
+            updateHistorySlider();
+        }
+    });
+
+    window.addEventListener("resize", updateHistorySlider);
+    updateHistorySlider();
 });
